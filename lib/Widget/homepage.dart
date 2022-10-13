4c8d0a17e3e232a1bf8e2512f061006637666cfa
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-13 19:31:50 +0100

    Updating Row buttons

--- lib/Widget/homepage.dart
+++ lib/Widget/homepage.dart
@@ -10,17 +10,54 @@ class Stopwatch extends StatefulWidget {
 class _StopwatchState extends State<Stopwatch> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF1c2757),
+    return Scaffold(
+      backgroundColor: const Color(0xFF1c2757),
       body: SafeArea(
           child: Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Center(
-                    child: Text('StopWatch App', style: TextStyle(color: )),)
+                  const Center(
+                    child: Text('StopWatch App',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Container(
+                      height: 400.0,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF323F68),
+                        borderRadius: BorderRadius.circular(8.0),
+                      )),
+                  const SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          child: RawMaterialButton(
+                              onPressed: () {},
+                              fillColor: Colors.blue,
+                              shape: const StadiumBorder(),
+                              child: const Text(
+                                'Start',
+                                style: TextStyle(color: Colors.white),
+                              ))),
+                      Expanded(
+                          child: RawMaterialButton(
+                              onPressed: () {},
+                              shape: const StadiumBorder(
+                                side: BorderSide(color: Colors.blue),
+                              ),
+                              child: const Text(
+                                'Reset',
+                                style: TextStyle(color: Colors.white),
+                              )))
+                    ],
+                  )
                 ],
               ))),
     );
